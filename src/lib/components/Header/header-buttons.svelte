6dfce948164268e000d433f2d3b73e10abad41d5
Enes Buzluk <139135923+cysctl@users.noreply.github.com>
2026-03-29 17:54:31 +0300

    chore: ConnectionStatus, HeaderButtons, ToggleTheme, WsSettings components (not completed)

---
+++ src/lib/components/Header/header-buttons.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import ToggleTheme from './toggle-theme.svelte';
+	import WsSettings from './ws-settings.svelte';
+</script>
+
+<div class="flex items-center gap-4">
+	<ToggleTheme />
+	<WsSettings />
+</div>
--- src/lib/components/Header/index.svelte
+++ src/lib/components/Header/index.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	import ConnectionStatus from './connection-status.svelte';
+	import HeaderButtons from './header-buttons.svelte';
 	import Logo from './logo.svelte';
 </script>
 
 <header class="flex items-center justify-between py-4">
 	<Logo />
+	<section class="flex items-center gap-4">
+		<ConnectionStatus />
+		<HeaderButtons />
+	</section>
 </header>
