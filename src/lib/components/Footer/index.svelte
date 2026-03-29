46bc73e9c6c002040b74ae23002f16a89aaf418f
Enes Buzluk <139135923+cysctl@users.noreply.github.com>
2026-03-29 18:30:14 +0300

    feat: complete Footer component

--- src/lib/components/Footer/index.svelte
+++ src/lib/components/Footer/index.svelte
@@ -1,3 +1,21 @@
-<footer class="py-2">
-	<p>Footer</p>
+<footer class="border-t border-border bg-background py-6 text-center text-sm text-muted-foreground">
+	<div class="container mx-auto px-4 sm:px-6 lg:px-8">
+		<p class="mb-2">
+			This is a Proof of Concept (PoC) project. The goal is to fetch a live list of all Satellites
+			in a Constellation group and send commands to them.
+		</p>
+		<p class="mb-4">It is a simplified web interface for the MissionControl application.</p>
+		<p class="text-xs font-medium">
+			Developed by <span class="text-foreground">
+				<a
+					href="https://github.com/cysctl"
+					class="hover:text-foreground/80"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Enes Buzluk (cysctl)
+				</a>
+			</span>.
+		</p>
+	</div>
 </footer>
